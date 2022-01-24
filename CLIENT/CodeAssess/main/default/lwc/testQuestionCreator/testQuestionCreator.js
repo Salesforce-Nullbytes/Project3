4c5b1b00c030e801d2cda1bef9304b7333508e09
Nicholas Yoon <nyoon8360@gmail.com>
2022-01-24 15:17:26 -0500

    updated dependencies for codeassess package and started implementation of testQuestionCreator

--- CLIENT/CodeAssess/main/default/lwc/testQuestionCreator/testQuestionCreator.js
+++ CLIENT/CodeAssess/main/default/lwc/testQuestionCreator/testQuestionCreator.js
@@ -1,5 +1,48 @@
 import { LightningElement } from 'lwc';
+import InsertQuestion from '@salesforce/apex/testQuestionCreatorController.insertQuestion';
+import CheckNameCollision from '@salesforce/apex/testQuestionCreatorController.checkNameCollision';
 
 export default class TestQuestionCreator extends LightningElement {
-    
+    //holds uploaded file's data
+    fileData;
+
+    fileTopic;
+
+    recordId;
+
+    handleFileUpload(event) {
+        //instantiate file variable and reader instance
+        const uFile = event.target.files[0];
+        let reader = new FileReader();
+        reader.onload = () => {
+            let base64 = reader.result.split(',')[1];
+            this.fileData = {
+                'fileName': uFile.name,
+                'base64': base64
+            }
+            console.log(this.fileData);
+        }
+        reader.readAsDataURL(this.fileData);
+    }
+
+    get topicOptions() {
+        //implement this once ca
+    }
+
+    handleTopicSelection(event) {
+        this.fileTopic = event.detail.fileTopic;
+    }
+
+    handleSubmitClicked() {
+        const {base64, filename, recordId} = this.fileData;
+
+        CheckNameCollision({name: this.fileTopic + '_' + this.fileData['fileName']}).then(result => {
+            //implement code for when naming collision exists
+            return;
+        });
+
+        InsertQuestion({base64, filename, recordId}).then(result => {
+
+        });
+    }
 }
