29caa44cbe33c8f54470caf85069e2f5bc19d164
Rebecca <rebeccahahn40@gmail.com>
2022-01-24 15:15:59 -0500

    Pulling fields from sObjects for components (work in progress)

--- CLIENT/CodeAssess/main/default/lwc/candidateInformation/candidateInformation.js
+++ CLIENT/CodeAssess/main/default/lwc/candidateInformation/candidateInformation.js
@@ -1,7 +1,37 @@
-import {LightningElement} from 'lwc';
+import {LightningElement, api} from 'lwc';
+import getCandidateInfo from '@salesforce/apex/individualResultsController.getCandidateInfo';
 
 export default class candidateInformation extends LightningElement {
+    @api recordId;
+
     candidateName = 'Candidate Name';
     candidateEmail = 'Candidate Email';
     candidatePhoneNumber = 'Candidate Phone Number';
+
+    componentRendered = false;
+
+    loadInfo() {
+        getCandidateInfo({record : this.recordId})
+        .then((result) => {
+            console.log("Success");
+
+            this.candidateName = result.Candidate__r.Name;
+            //this.candidateEmail = Candidate__c.Email__c;
+            //this.candidatePhoneNumber = Candidate__c.Phone_Number__c;
+        })
+        .catch((error) => {
+            console.log("Failure");
+        });
+    }
+
+
+    renderedCallback() {
+        if (this.componentRendered) {
+            return;
+        } 
+
+        this.loadInfo();
+
+        this.componentRendered = true;
+    }
 }
