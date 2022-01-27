eb5f185f4cd0c62230330b94de3390ab7a070645
Rebecca <rebeccahahn40@gmail.com>
2022-01-27 14:26:58 -0500

    Changes to individual candidate view lwc and controller

--- CLIENT/CodeAssess/main/default/lwc/candidateSummary/candidateSummary.js
+++ CLIENT/CodeAssess/main/default/lwc/candidateSummary/candidateSummary.js
@@ -4,15 +4,13 @@ import getCandidateSummary from '@salesforce/apex/candidateInformationController
 export default class candidateSummary extends LightningElement {
     @api recordId;
 
+    score = 0;
     averageScore = 0;
     maxScore = 0;
     minScore = 100;
     summaryPassFail = 'Fail';
 
     sum = 0;
-
-    totalTime = 0;
-    averageTime = 0;
 
     componentRendered = false;
 
@@ -21,15 +19,16 @@ export default class candidateSummary extends LightningElement {
         .then((result) => {
 
             for (let i = 0; i < result.length; i++) {
- 
-                this.sum += result[i].CodeCoverage__c;
 
-                if (result[i].CodeCoverage__c > this.maxScore) {
-                    this.maxScore = result[i].CodeCoverage__c;
+                this.score = result[i].MethodsPassed__c / result[i].TotalMethods__c;
+                this.sum +=  this.score;
+
+                if (this.score > this.maxScore) {
+                    this.maxScore = this.score;
                 }
 
-                if (result[i].CodeCoverage__c < this.minScore) {
-                   this.minScore = result[i].CodeCoverage__c;
+                if (this.score < this.minScore) {
+                   this.minScore = this.score;
                 }
 
             }
--- CLIENT/CodeAssess/main/default/lwc/candidateTestResults/candidateTestResults.js
+++ CLIENT/CodeAssess/main/default/lwc/candidateTestResults/candidateTestResults.js
@@ -29,23 +29,23 @@ export default class candidateTestResults extends LightningElement {
         .then((result) => {
 
             this.testOneTopic = result[0].Question__r.QuestionTopic__c;
-            this.testOnePercentage = result[0].CodeCoverage__c;
+            this.testOnePercentage = result[0].MethodsPassed__c / result[0].TotalMethods__c;
             this.testOnePassFail = result[0].Result__c;
 
             this.testTwoTopic = result[1].Question__r.QuestionTopic__c;
-            this.testTwoPercentage = result[1].CodeCoverage__c;
+            this.testTwoPercentage = result[1].MethodsPassed__c / result[1].TotalMethods__c;
             this.testTwoPassFail = result[1].Result__c;
 
             this.testThreeTopic = result[2].Question__r.QuestionTopic__c;
-            this.testThreePercentage = result[2].CodeCoverage__c;
+            this.testThreePercentage = result[2].MethodsPassed__c / result[2].TotalMethods__c;
             this.testThreePassFail = result[2].Result__c;
 
             this.testFourTopic = result[3].Question__r.QuestionTopic__c;
-            this.testFourPercentage = result[3].CodeCoverage__c;
+            this.testFourPercentage = result[3].MethodsPassed__c / result[3].TotalMethods__c;
             this.testFourPassFail = result[3].Result__c;
 
             this.testFiveTopic = result[4].Question__r.QuestionTopic__c;
-            this.testFivePercentage = result[4].CodeCoverage__c;
+            this.testFivePercentage = result[4].MethodsPassed__c / result[4].TotalMethods__c;
             this.testFivePassFail = result[4].Result__c;          
         })
         .catch((error) => {
