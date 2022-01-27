d9d6c6234547ffcc71667c94891a1f8dfaa9d032
Supriya Ranjan <supriyaranjan.mit>
2022-01-26 23:51:11 -0600

    Modified codeAssessor

--- CANDIDATE/ApplicantEnvironment/main/default/lwc/codeAssessor/codeAssessor.js
+++ CANDIDATE/ApplicantEnvironment/main/default/lwc/codeAssessor/codeAssessor.js
@@ -4,13 +4,18 @@ export default class CodeAssessor extends LightningElement {
 
     credentials = false;
 
-    getUserDetails(event)
+    /*getUserDetails(event)
     {
         let userDetails = event.detail;
         console.log("user ID:" + userDetails[0].Id);
         console.log("user Name:" + userDetails[0].name);
         //Once user details are received, credentials need to be set to true
         //this.credentials = true;
+    }*/
+
+    handleUserValidated(event)
+    {
+        this.credentials = event.detail;
     }
 
 }
