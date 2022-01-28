502e6a8c1b9fd6cfc6369186563df6e63da97007
Matari Howard <howardmatari@gmail.com>
2022-01-28 11:59:44 -0500

    add fields to candidate result object

--- CLIENT/CodeAssess/main/default/lwc/candidate/candidate.js
+++ CLIENT/CodeAssess/main/default/lwc/candidate/candidate.js
@@ -9,20 +9,20 @@ const FIELDS = [
 ];
 
 export default class Candidate extends LightningElement {
-    @api recordId;
+    //@api recordId;
 
-    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
-    candidate__c;
+//     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+//     candidate__c;
 
-    get name() {
-        return this.candidate__c.data.fields.Name.value;
-    }
+//     get name() {
+//         return this.candidate__c.data.fields.Name.value;
+//     }
 
-    get phonenumber() {
-        return this.candidate__c.data.fields.PhoneNumber__C.value;
-    }
+//     get phonenumber() {
+//         return this.candidate__c.data.fields.PhoneNumber__C.value;
+//     }
 
-    get email() {
-        return this.candidate__c.data.fields.Email__c.value;
-    }
-}
+//     get email() {
+//         return this.candidate__c.data.fields.Email__c.value;
+//     }
+ }
